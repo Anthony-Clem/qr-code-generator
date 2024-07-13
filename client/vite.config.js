9a95eefe67327d2fe8b93cd3ade18b197209cef9
Anthony Clement <antclem6@gmail.com>
2024-07-13 17:03:33 -0400

    MIME issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
   },
-  base: "/",
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    mimeTypes: {
+      "application/javascript": ["js", "mjs"],
+    },
+  },
 });
